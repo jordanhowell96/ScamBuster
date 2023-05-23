00d8ae1b69494a67f8b49811b543335a782d6088
jordanhowell96 <howell.a.jordan@gmail.com>
2023-05-22 20:07:46 -0700

    Added extra console info

--- microservices/detection_micro/send_email.py
+++ microservices/detection_micro/send_email.py
@@ -9,8 +9,9 @@ socket.connect("tcp://localhost:5555")
 def send_email(email, subject, message):
     socket.send(json.dumps({"email": email, "subject": subject, "message": message}).encode())
     response = socket.recv().decode()
-    print(response)
+    print("Response:", response)
 
 
 if __name__ == "__main__":
+    print("Sending Test Email")
     send_email('test.user@domain.com', '(Test) Scam Detected', 'This is a test message for a detected scam.')
